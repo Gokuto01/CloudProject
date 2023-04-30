ed43ada6ae429ce1133db011b09365c63b91009c
Gokuto Kung <73678927+Gokuto01@users.noreply.github.com>
2023-04-30 10:11:19 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,13 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const connection = mysql.createConnection({
   host: 'database-1.c4ge7ygbihr4.us-east-1.rds.amazonaws.com',
   user: 'root',
